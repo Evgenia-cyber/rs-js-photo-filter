6afe8cf610d4ef81c6238129af88fd88b0a3ffd8
Evgenia Kurineva <kurineva85@mail.ru>
2021-08-19 10:19:45 +0300

    feat: add blur filter

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log(1);
+const inputs = document.querySelectorAll('.filter');
+
+function handleChange(event) {
+  const value = this.value;
+
+  const filterValueEl = event.target.parentNode.lastElementChild;
+  filterValueEl.textContent = value;
+
+  const size = this.dataset.size || '';
+
+  document.documentElement.style.setProperty(`--${this.id}`, value + size);
+}
+
+inputs.forEach((input) => input.addEventListener('change', handleChange));
