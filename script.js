455cce76d5a90ab1190fca8f7b4578bbf5f738d4
Evgenia Kurineva <kurineva85@mail.ru>
2021-08-24 17:37:16 +0300

    feat: add presets

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 const inputs = document.querySelectorAll('.filter');
+const presets = document.querySelectorAll('.presets-img');
+const initFilters = {
+  blur: [0, 'px'],
+  brightness: [100, '%'],
+  contrast: [100, '%'],
+  grayscale: [0, '%'],
+  'hue-rotate': [0, 'deg'],
+  invert: [0, '%'],
+  opacity: [100, '%'],
+  saturate: [100, '%'],
+  sepia: [0, '%'],
+};
 
 function handleChange(event) {
   const value = this.value;
@@ -11,4 +23,54 @@ function handleChange(event) {
   document.documentElement.style.setProperty(`--${this.id}`, value + size);
 }
 
+function handleOnPresetClick() {
+  resetFilters();
+
+  const { filter } = window.getComputedStyle(this); // 'brightness(0.81) saturate(1.9)'
+
+  filter.split(' ').forEach((item) => {
+    const arr = item.split('(');
+
+    const filterName = arr[0];
+
+    const filterValue =
+      filterName === ('blur' && 'hue-rotate')
+        ? parseFloat(arr[1])
+        : Math.round(parseFloat(arr[1]) * 100);
+
+    // const filterSize =
+    //   filterName === 'blur' ? 'px' : filterName === 'hue-rotate' ? 'deg' : '%';
+    const filterSize = initFilters[filterName][1];
+
+    const inputEl = document.querySelector(`#${filterName}`);
+    inputEl.value = filterValue;
+
+    const filterValueEl = document.querySelector(`.filter-value-${filterName}`);
+    filterValueEl.textContent = filterValue;
+
+    document.documentElement.style.setProperty(
+      `--${filterName}`,
+      filterValue + filterSize,
+    );
+  });
+}
+
+function resetFilters() {
+  Object.keys(initFilters).forEach((filter) => {
+    const filterValue = initFilters[filter][0];
+    const filterSize = initFilters[filter][1];
+
+    const filterValueEl = document.querySelector(`.filter-value-${filter}`);
+    filterValueEl.textContent = filterValue;
+
+    document.documentElement.style.setProperty(
+      `--${filter}`,
+      filterValue + filterSize,
+    );
+  });
+}
+
 inputs.forEach((input) => input.addEventListener('change', handleChange));
+presets.forEach((preset) =>
+  preset.addEventListener('click', handleOnPresetClick),
+);
