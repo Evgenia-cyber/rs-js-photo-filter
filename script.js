208cbfbfd350e3a62c2d1855f9e639d58c5f0a5d
Evgenia Kurineva <kurineva85@mail.ru>
2021-08-27 14:31:30 +0300

    feat: add save button

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const initFilters = {
 
 const initAll = {
   ...initFilters,
-  size: [750, 'px'],
+  size: [1, ''],
   'bg-color': ['#1a1920', ''],
 };
 
@@ -23,6 +23,8 @@ const resetFiltersBtn = document.querySelector('.reset-filters-btn');
 
 const imgEl = document.querySelector('.img');
 const inputLoadFileEl = document.querySelector('.load-file-input');
+
+const saveBtn = document.querySelector('.save-btn');
 
 function handleChange(event) {
   const value = this.value;
@@ -97,6 +99,21 @@ function handleOnLoadImg() {
   reader.readAsDataURL(imgFile);
 }
 
+function handleOnSaveBtnClick() {
+  const canvas = document.createElement('canvas');
+  canvas.width = imgEl.clientWidth;
+  canvas.height = imgEl.clientHeight;
+  const ctx = canvas.getContext('2d');
+  const { filter } = window.getComputedStyle(imgEl);
+  ctx.filter = filter; // 'blur(0px) brightness(1) contrast(1) grayscale(0) hue-rotate(0deg) invert(0) opacity(1) saturate(1) sepia(0)';
+  ctx.drawImage(imgEl, 0, 0, canvas.width, canvas.height);
+  const url = canvas.toDataURL('image/jpg');
+  const linkToSave = document.querySelector('.save-file');
+  linkToSave.download = 'image.jpg';
+  linkToSave.href = url;
+  canvas.delete;
+}
+
 inputs.forEach((input) => input.addEventListener('change', handleChange));
 
 presets.forEach((preset) =>
@@ -108,3 +125,5 @@ resetAllBtn.addEventListener('click', handleOnResetAllBtnClick);
 resetFiltersBtn.addEventListener('click', handleOnResetFiltersBtnClick);
 
 inputLoadFileEl.addEventListener('change', handleOnLoadImg);
+
+saveBtn.addEventListener('click', handleOnSaveBtnClick);
